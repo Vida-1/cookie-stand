10b9c327f149ee6e2886159b2b212b48ac58510d
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2022-04-24 16:57:31 -0400

    finished requirements of lab7. pushing for canvas submission.

--- js/app.js
+++ js/app.js
@@ -20,12 +20,11 @@ function StoreConstructor(name, minHourlyCustomers, maxHourlyCustomers, avgCooki
     this.randomCustomersPerHour = [];
     this.grandTotalCookies = [];
     allStores.push(this);
-    //   this.tableReadout = renderToDom(); -did not work
 };
 
 StoreConstructor.prototype.generateRandomCustomersPerHour = function () {
     for (let i = 0; i < hours.length; i++) {
-        let customersPerHour = Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers);
+        let customersPerHour = (Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1) + this.minHourlyCustomers);
         this.randomCustomersPerHour.push(Math.floor(customersPerHour));
     }
 };
@@ -45,52 +44,92 @@ StoreConstructor.prototype.generateGrandTotalCookies = function () {
     this.grandTotalCookies.push(totalCookieAccumulator);
 };
 
-// Code Fellows TA Justin helped me with these next two arrow functions
+StoreConstructor.prototype.renderRowToDom = function () {
 
+    const ulElem = document.createElement('tr');
+    articleElem.appendChild(ulElem);
+    const liElemFirst = document.createElement('td');
+    ulElem.appendChild(liElemFirst)
+    liElemFirst.textContent = this.name;
+
+    // table data cells
+    for (let i = 0; i < hours.length; i++) {
+        const liElem = document.createElement('td');
+        ulElem.appendChild(liElem);
+        const cookiesSoldThisHour = this.simulatedCookiesPurchasedEachHour[i];
+        liElem.textContent = `${cookiesSoldThisHour}`;
+    }
+
+    // total sales column on the right
+    const liElem = document.createElement('td');
+    ulElem.appendChild(liElem);
+    liElem.textContent = this.grandTotalCookies;
+
+};
+
+
+//MAIN LOGIC STARTS HERE
+
+// Code Fellows TA Justin helped me with these next two arrow functions
 locationCatalog.forEach(location => {
     new StoreConstructor(location[0], location[1], location[2], location[3]);
 })
 console.log(allStores);
 
-StoreConstructor.prototype.renderToDom = function () {
-    // get on screen
-    const containerElem = document.getElementById("franchiseLocations");
+// get on screen
+const containerElem = document.getElementById("franchiseLocations");
 
+// create a table
+const articleElem = document.createElement('Table');
+containerElem.appendChild(articleElem);
 
-    // article
-    const articleElem = document.createElement('article');
-    console.log(JSON.stringify(articleElem, null, 4));
-    containerElem.appendChild(articleElem);
+// create a table header
+const headingElem = document.createElement('tr');
+articleElem.appendChild(headingElem);
 
-    // heading = this prints the name of the store location
-    const headingElem = document.createElement('h2');
-    articleElem.appendChild(headingElem);
-    headingElem.textContent = this.name;
+const hourDatumFirst = document.createElement('th');
+headingElem.appendChild(hourDatumFirst);
+hourDatumFirst.textContent = "City";
 
-    // ul
-    const ulElem = document.createElement('ul');
-    articleElem.appendChild(ulElem);
-
-    // list items
-    for (let i = 0; i < hours.length; i++) {
-        const liElem = document.createElement('li');
-        ulElem.appendChild(liElem);
-        const timeSlot = hours[i];
-        const cookiesSoldThisHour = this.simulatedCookiesPurchasedEachHour[i];
-        liElem.textContent = `${timeSlot}: ${cookiesSoldThisHour}`;
-    }
-
-    // total sales
-    const liElem = document.createElement('li');
-    ulElem.appendChild(liElem);
-    liElem.textContent = 'Total: ' + this.grandTotalCookies;
-};
+for (let i = 0; i < hours.length; i++) {
+    const hourDatum = document.createElement('th');
+    headingElem.appendChild(hourDatum);
+    hourDatum.textContent = hours[i];
+}
+const hourDatumLast = document.createElement('th');
+headingElem.appendChild(hourDatumLast);
+hourDatumLast.textContent = "Total";
 
 allStores.forEach(store => {
-
     store.generateRandomCustomersPerHour();
     store.generateSimulatedCookiesPurchasedEachHour();
     store.generateGrandTotalCookies();
-    store.renderToDom();
-
+    store.renderRowToDom();
 });
+
+// create a table footer
+// const footerElem = document.createElement('tfoot');
+// .appendChild(footerElem);
+// footerElem.textContent('this is the footer');                                      // PLACEHOLDER TEXT WRITE FXN FOR THIS 
+
+
+//alternate strategy psuedocode
+//populate store information to all stores  (ln 48 - 53 - globally being
+//  create table (ln 54-59)
+//  create a header row (doesnt exist yet but likely starts at 61 - 64)
+//  for each store in allstores (ln 91-96)
+//      create a row (ln 95 renderRowToDom)
+//      populate name and hours as cell data (see render row to dom function)
+//      populate cookies total as final cell in row (see end of render row to dom function)
+//  create final row with totals (doesnt exist yet)
+
+
+
+    // // list items -- junk pile
+    // for (let i = 0; i < hours.length; i++) {
+    //     const liElem = document.createElement('td');
+    //     ulElem.appendChild(liElem);
+    //     const storeName = this.name;
+    //     const timeSlot = hours[i];
+    //     const cookiesSoldThisHour = this.simulatedCookiesPurchasedEachHour[i];
+    //     liElem.textContent = `${timeSlot}: ${cookiesSoldThisHour}`;
